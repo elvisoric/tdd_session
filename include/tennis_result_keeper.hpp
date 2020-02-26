f8ca2f7e4a53d5154b5a9ad7db5b5c93c94e9b42
Elvis Oric <elvis.oric90@gmail.com>
2020-02-26 15:56:48 +0100

    Add two test fixtures with some tests to support game points changes.

--- include/tennis_result_keeper.hpp
+++ include/tennis_result_keeper.hpp
@@ -1,9 +1,46 @@
 #ifndef TDD_TENNIS_RESULT_KEEPER_HPP
 #define TDD_TENNIS_RESULT_KEEPER_HPP
 
-namespace tdd{
+#include <string>
 
+namespace tdd {
 
-}
+struct TennisResultKeeper {
+  inline std::string result() const {
+    std::string first, second;
+    if (firstPoints == 55) {
+      first = "A";
+      second = "-";
+    } else if (secondPoints == 55) {
+      first = "-";
+      second = "A";
+    } else {
+      first = std::to_string(firstPoints);
+      second = std::to_string(secondPoints);
+    }
+    return "| 0 | 0 | " + first + " |\n| 0 | 0 | " + second + " |";
+  }
 
-#endif // TDD_TENNIS_RESULT_KEEPER_HPP
+  inline void pointFirstPlayer() {
+    if (secondPoints == 55) {
+      secondPoints = 40;
+    } else {
+      firstPoints == 30 ? firstPoints = 40 : firstPoints += 15;
+    }
+  }
+
+  inline void pointSecondPlayer() {
+    if (firstPoints == 55) {
+      firstPoints = 40;
+    } else {
+      secondPoints == 30 ? secondPoints = 40 : secondPoints += 15;
+    }
+  }
+
+  int firstPoints{0};
+  int secondPoints{0};
+};
+
+}  // namespace tdd
+
+#endif  // TDD_TENNIS_RESULT_KEEPER_HPP
--- test/tennis_result_keeper_test.cpp
+++ test/tennis_result_keeper_test.cpp
@@ -4,6 +4,129 @@
 
 using namespace ::testing;
 
-TEST(TennisResultKeeperTest, NameOfFirstFailingTest){
-  ASSERT_TRUE(false);
+namespace {
+class TennisResultKeeperTest : public testing::Test {
+  protected:
+  tdd::TennisResultKeeper keeper;
+};
+
+}  // namespace
+
+TEST_F(TennisResultKeeperTest,
+       ExpectsThatWhenCreatingNewResultKeeperTheSetGameAndPointsAreZero) {
+  const std::string expected =
+      "| 0 | 0 | 0 |\n"
+      "| 0 | 0 | 0 |";
+  ASSERT_THAT(keeper.result(), Eq(expected));
+}
+
+TEST_F(TennisResultKeeperTest,
+       ExpectsThatAddingFirstPointForFirstPlayerIncreasesPointsTo15) {
+  keeper.pointFirstPlayer();
+  const std::string expected =
+      "| 0 | 0 | 15 |\n"
+      "| 0 | 0 | 0 |";
+  ASSERT_THAT(keeper.result(), Eq(expected));
+}
+
+TEST_F(TennisResultKeeperTest,
+       ExpectsThatAddingFirstPointForSecondPlayerIncreasesPointsTo15) {
+  keeper.pointSecondPlayer();
+  const std::string expected =
+      "| 0 | 0 | 0 |\n"
+      "| 0 | 0 | 15 |";
+  ASSERT_THAT(keeper.result(), Eq(expected));
+}
+
+TEST_F(TennisResultKeeperTest, ExpectsToConfirmPointsOrderingForBothPlayers) {
+  keeper.pointFirstPlayer();
+  const std::string expected1 =
+      "| 0 | 0 | 15 |\n"
+      "| 0 | 0 | 0 |";
+  EXPECT_THAT(keeper.result(), Eq(expected1));
+
+  keeper.pointFirstPlayer();
+  const std::string expected2 =
+      "| 0 | 0 | 30 |\n"
+      "| 0 | 0 | 0 |";
+  EXPECT_THAT(keeper.result(), Eq(expected2));
+
+  keeper.pointSecondPlayer();
+  keeper.pointSecondPlayer();
+  const std::string expected3 =
+      "| 0 | 0 | 30 |\n"
+      "| 0 | 0 | 30 |";
+  EXPECT_THAT(keeper.result(), Eq(expected3));
+
+  keeper.pointSecondPlayer();
+  const std::string expected4 =
+      "| 0 | 0 | 30 |\n"
+      "| 0 | 0 | 40 |";
+  EXPECT_THAT(keeper.result(), Eq(expected4));
+
+  keeper.pointFirstPlayer();
+  const std::string expected5 =
+      "| 0 | 0 | 40 |\n"
+      "| 0 | 0 | 40 |";
+  EXPECT_THAT(keeper.result(), Eq(expected5));
+
+  keeper.pointFirstPlayer();
+  const std::string expected6 =
+      "| 0 | 0 | A |\n"
+      "| 0 | 0 | - |";
+  EXPECT_THAT(keeper.result(), Eq(expected6));
+}
+
+namespace {
+
+class TennisResultKeeperWithAdvantageTest : public TennisResultKeeperTest {
+  public:
+  void SetUp() override {
+    keeper.pointFirstPlayer();
+    keeper.pointFirstPlayer();
+    keeper.pointFirstPlayer();
+    keeper.pointSecondPlayer();
+    keeper.pointSecondPlayer();
+    keeper.pointSecondPlayer();
+  }
+};
+}  // namespace
+
+TEST_F(TennisResultKeeperWithAdvantageTest,
+       ExpectsToConfirmThatFirstPlayerCanHaveAdvantage) {
+  keeper.pointFirstPlayer();
+
+  const std::string expected =
+      "| 0 | 0 | A |\n"
+      "| 0 | 0 | - |";
+  EXPECT_THAT(keeper.result(), Eq(expected));
+}
+
+TEST_F(TennisResultKeeperWithAdvantageTest,
+       ExpectsToConfirmThatSecondPlayerCanHaveAdvantage) {
+  keeper.pointSecondPlayer();
+
+  const std::string expected =
+      "| 0 | 0 | - |\n"
+      "| 0 | 0 | A |";
+  EXPECT_THAT(keeper.result(), Eq(expected));
+}
+
+TEST_F(TennisResultKeeperWithAdvantageTest,
+       ExpectsToConfirmThatResultIsDeuceWhenPlayerLosesAdvantage) {
+  keeper.pointFirstPlayer();
+  keeper.pointSecondPlayer();
+
+  const std::string expected1 =
+      "| 0 | 0 | 40 |\n"
+      "| 0 | 0 | 40 |";
+  EXPECT_THAT(keeper.result(), Eq(expected1));
+
+  keeper.pointSecondPlayer();
+  keeper.pointFirstPlayer();
+
+  const std::string expected2 =
+      "| 0 | 0 | 40 |\n"
+      "| 0 | 0 | 40 |";
+  EXPECT_THAT(keeper.result(), Eq(expected2));
 }
