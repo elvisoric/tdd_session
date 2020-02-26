07f6a3820ffbd1ba86cb7dd6f4095f66165c4a5f
Elvis Oric <elvis.oric90@gmail.com>
2020-02-26 13:30:54 +0100

    Prepare code base for tennis result keeper tests

---
+++ include/tennis_result_keeper.hpp
@@ -0,0 +1,9 @@
+#ifndef TDD_TENNIS_RESULT_KEEPER_HPP
+#define TDD_TENNIS_RESULT_KEEPER_HPP
+
+namespace tdd{
+
+
+}
+
+#endif // TDD_TENNIS_RESULT_KEEPER_HPP
---
+++ test/tennis_result_keeper_test.cpp
@@ -0,0 +1,9 @@
+#include <gmock/gmock.h>
+
+#include <tennis_result_keeper.hpp>
+
+using namespace ::testing;
+
+TEST(TennisResultKeeperTest, NameOfFirstFailingTest){
+  ASSERT_TRUE(false);
+}
